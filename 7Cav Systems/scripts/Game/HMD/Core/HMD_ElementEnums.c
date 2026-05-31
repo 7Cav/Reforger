2a8735c073bc22a8b5bb2e1d2220fa267ecf9109
Ryan Westcott <rwest311@gmail.com>
2026-05-31 17:57:32 -0500

    Fixes after 1.7 update

--- 7Cav Systems/scripts/Game/HMD/Core/HMD_ElementEnums.c
+++
@@ -1,7 +0,0 @@
-//------------------------------------------------------------------------------------------------
-//! Shared HMD pool element kind enum.
-enum EHmdElementKind
-{
-	INFORMATIONAL = 0,
-	TARGET_DESIGNATION = 1
-}
