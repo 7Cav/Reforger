e3a569d3f429b2def567f7615ebfac0c2cff5402
Ryan Westcott <rwest311@gmail.com>
2026-06-14 20:41:02 -0500

    Changes and bug fixes to Kits.

--- 7Cav Systems/scripts/Game/CAV_SLseesownSquadMarker.c
+++ 7Cav Systems/scripts/Game/CAV_SLseesownSquadMarker.c
@@ -1,16 +1,1 @@
-modded class SCR_MapMarkerSquadLeader
-{
-	override void OnPlayerIdUpdate()
-	{
-		PlayerController pController = GetGame().GetPlayerController();
-		if (!pController)
-			return;
-		
-		if (m_PlayerID == pController.GetPlayerId())	// if this is us, dont display
-			SetLocalVisible(true); // set this true for markers testing
-		else
-		{
-			SetLocalVisible(true);
-		}
-	}
-}
+// Deprecated: moved to Scripts/Game/CAV_SLseesownSquadMarker.c
